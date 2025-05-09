0d1b1ffad1ce7a1e79525bc833b84553d528947a
ArJKloek <123403478+ArJKloek@users.noreply.github.com>
2025-05-09 13:50:08 +0200

    Update plot_manager.py

--- scripts/plot_manager.py
+++ scripts/plot_manager.py
@@ -221,62 +221,29 @@ class PlotManager:
         self.update_plot_from_log(plot_index=1, curve_configs=curve_configs, show_right_axis=show_temp)
 
 
-    def update_dual_plot_old(self):
-        widget = self.main.graphWidgets[1]
-        if self.main.temp_curve is not None:
-            self.main.tempViewBox.removeItem(self.main.temp_curve)
-            self.main.temp_curve = None
-
-        if self.main.pH_curve is not None:
-            widget.removeItem(self.main.pH_curve)
-            self.main.pH_curve = None
-
-        ph_time, ph_data = read_log_data(self.main, 1)
-        temp_time, temp_data = read_log_data(self.main, 2)
-        # Filter out "N/A" or invalid data
-        
-        #ph_data = [(t, float(v)) for t, v in zip(ph_time, ph_data_raw) if isinstance(v, (int, float)) or self._is_number(v)]
-        
-        #temp_data = [(t, float(v)) for t, v in zip(temp_time, temp_data_raw) if isinstance(v, (int, float)) or self._is_number(v)]
-        
-        max_points = 1000
-
-        if ph_time and ph_data:
-            x_ph, y_ph, time_label, _ = scale_time_data(self.main, ph_time, ph_data)
-            widget.setLabel('bottom', f'Time ({time_label})', **self.labelStyle)
-
-            if len(x_ph) > max_points:
-                x_ph = x_ph[-max_points:]
-                y_ph = y_ph[-max_points:]
-
-            self.main.pH_curve = widget.plot(x_ph, y_ph, pen=pg.mkPen('g', width=2))
-        
-        if self.main.toggleTempAction.isChecked():
-            if temp_time and temp_data:
-                x_temp, y_temp, _, _ = scale_time_data(self.main, temp_time, temp_data)
-
-                if len(x_temp) > max_points:
-                    x_temp = x_temp[-max_points:]
-                    y_temp = y_temp[-max_points:]
-
-                self.main.temp_curve = pg.PlotCurveItem(x_temp, y_temp, pen=pg.mkPen('r', width=2))
-                self.main.tempViewBox.addItem(self.main.temp_curve)
-                self.main.tempViewBox.enableAutoRange(axis=pg.ViewBox.YAxis)
-            self.main.graphWidgets[1].showAxis('right')
-        else:
-            # Remove temp curve if it's showing
-            if self.main.temp_curve:
-                self.main.tempViewBox.removeItem(self.main.temp_curve)
-                self.main.temp_curve = None
-
-            # Hide the right axis
-            self.main.graphWidgets[1].hideAxis('right')
-
     def update_power_plot(self):
-        if hasattr(self.main, 'voltage_log') and self.main.voltage_log:
-            times, volts = zip(*self.main.voltage_log)
-            times = np.array(times) - times[0]
-            self.main.volt_curve.setData(times, volts)
+        curve_configs = [
+            {
+                "log_index": 3,
+                "curve_attr": "Volt_curve",
+                "pen": "g",
+                "use_right_axis": False
+            }
+        ]
+
+        # Only show temperature if enabled
+        #if self.main.toggleTempAction.isChecked():
+        curve_configs.append({
+            "log_index": 4,
+            "curve_attr": "amp_curve",
+            "pen": "r",
+            "use_right_axis": True
+        })
+        show_amp = True
+       
+
+        self.update_plot_from_log(plot_index=2, curve_configs=curve_configs, show_right_axis=show_amp)
+
 
         if hasattr(self.main, 'current_log') and self.main.current_log:
             times, amps = zip(*self.main.current_log)
