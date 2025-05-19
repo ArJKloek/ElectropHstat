84e4439e167dac6e4293b336898f35ff14fa3df2
ArJKloek <123403478+ArJKloek@users.noreply.github.com>
2025-05-19 12:57:33 +0200

    Update pps_connections.py

--- electrophstat/connections/pps_connections.py
+++ electrophstat/connections/pps_connections.py
@@ -9,13 +9,13 @@ class PPSConnections(QObject):
     def __init__(self, window):
         super().__init__(window)
         self.win = window
-        self.pps_ctrl = None
+        self.pps_worker = None
         #self.win.powerButton.clicked.connect(self.togglePowerSupply)
         self.win.setButton.clicked.connect(self.apply_ps_settings)
 
     def set_worker(self, worker):
         self.pps_worker = worker
-        
+
     @pyqtSlot(float)
     def update_pps_voltage(self, voltage: float):
         self.win.voltagelabel.setText(f"{voltage:.1f} V")
@@ -38,7 +38,7 @@ class PPSConnections(QObject):
     
     @pyqtSlot()
     def apply_ps_settings(self):
-        if not self.pps_ctrl:
+        if not self.pps_worker:
             print("[WARNING] PPS worker not initialized.")
             return
 
@@ -47,23 +47,23 @@ class PPSConnections(QObject):
         current = self.win.currentDial.value() / 10.0
 
         if mode == "CV":
-            self.pps_ctrl.set_current(self.pps_ctrl.psu.IMAX)
-            self.pps_ctrl.set_voltage(voltage)
+            self.pps_worker.set_current(self.pps_worker.psu.IMAX)
+            self.pps_worker.set_voltage(voltage)
         else:  # "CC"
-            self.pps_ctrl.set_voltage(self.pps_ctrl.psu.VMAX)
-            self.pps_ctrl.set_current(current)
+            self.pps_worker.set_voltage(self.pps_worker.psu.VMAX)
+            self.pps_worker.set_current(current)
 
         print(f"[SET] Mode: {mode}, Voltage: {voltage:.1f} V, Current: {current:.1f} A")
 
     @pyqtSlot()
     def togglePowerSupply(self):
-        if not self.pps_ctrl:
+        if not self.pps_worker:
             QMessageBox.warning(self.win, "PPS Error", "Power supply is not connected.")
             self.win.powerButton.setChecked(False)
             return
         state = self.win.powerButton.isChecked()
         try:
-            self.pps_ctrl.set_output(state)
+            self.pps_worker.set_output(state)
             print(f"[OUTPUT] Power Supply set to {'ON' if state else 'OFF'}")
         except Exception as e:
             QMessageBox.critical(self.win, "PPS Error", f"Could not set output: {e}")
