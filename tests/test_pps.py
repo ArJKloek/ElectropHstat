5d46e085824aee68d210f85d3376d4c1dc16d491
ArJKloek <123403478+ArJKloek@users.noreply.github.com>
2025-05-19 10:22:15 +0200

    Create test_pps.py

---
+++ tests/test_pps.py
@@ -0,0 +1,19 @@
+from electrophstat.hardware.voltcraft_pps import VoltcraftPPS
+
+psu = VoltcraftPPS('/dev/ttyUSB0', reset=True, debug=True)
+psu.connect()
+
+try:
+    psu.voltage(5.0)
+    psu.current(1.0)
+    psu.output(True)
+
+    v, a, mode = psu.reading()
+    print(f"Voltage={v} V, Current={a} A, Mode={mode}")
+
+except Exception as e:
+    print("Error:", e)
+
+finally:
+    psu.output(False)
+    psu.disconnect()
