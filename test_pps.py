3b20da4d6b2e13d7f6c5e138355866698e00fad0
ArJKloek <123403478+ArJKloek@users.noreply.github.com>
2025-05-19 11:56:59 +0200

    Update test_pps.py

--- test_pps.py
+++ test_pps.py
@@ -4,9 +4,10 @@ psu = VoltcraftPPS('/dev/ttyUSB0', reset=True)
 psu.connect()
 
 try:
+    psu.output(1)
     psu.voltage(5.0)
     psu.current(1.0)
-    psu.output(True)
+    #psu.output(True)
 
     v, a, mode = psu.reading()
     print(f"Voltage={v} V, Current={a} A, Mode={mode}")
