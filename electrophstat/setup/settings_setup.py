ee51d35b263a1fec495ba973bc6c8988dfedb3d1
ArJKloek <123403478+ArJKloek@users.noreply.github.com>
2025-05-24 20:04:40 +0200

    Update settings_setup.py

--- electrophstat/setup/settings_setup.py
+++ electrophstat/setup/settings_setup.py
@@ -4,7 +4,7 @@ from electrophstat.gui.dialogs import SettingsDialog
 def init_settings(self):
 
     self.settings_dialog =  SettingsDialog(self)
-    self.actionSettings.clicked.connect(self.openSettingsDialog)
+    self.actionSettings.triggered.connect(self.openSettingsDialog)
 
 def openSettingsDialog(self):
     self.settings_dialog.exec_()
