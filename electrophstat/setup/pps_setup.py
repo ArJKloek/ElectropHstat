ebfc42c764a85d591453f2890380f241966792c0
ArJKloek <123403478+ArJKloek@users.noreply.github.com>
2025-05-24 12:53:07 +0200

    Update pps_setup.py

--- electrophstat/setup/pps_setup.py
+++ electrophstat/setup/pps_setup.py
@@ -22,18 +22,11 @@ def init_psu(win):
         )
         # show & enable all PSU UI
         win.PowerGroup.setVisible(True)
-        win.togglePsuButton.setEnabled(True)
-        win.actionEnable_PSU_control.setEnabled(True)
-        win.reconnect_pps_action.setEnabled(True)
+
     else:
         # tear it all down
         win.pps_connections = None
         win.pps_ctrl = None
         win.PowerGroup.setVisible(False)
-        win.togglePsuButton.setChecked(False)
-        win.togglePsuButton.setEnabled(False)
-        win.actionEnable_PSU_control.setChecked(False)
-        win.actionEnable_PSU_control.setEnabled(False)
-        win.reconnect_pps_action.setEnabled(False)
         # disable PSU logging
         win.logging_ctrl.disable_logging(['voltage', 'current', 'coulomb'])
