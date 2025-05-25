6b183a3ac9b02c9b2af69e69826303347e6b4459
ArJKloek <123403478+ArJKloek@users.noreply.github.com>
2025-05-25 12:13:29 +0200

    Update sensor_setup.py

--- electrophstat/setup/sensor_setup.py
+++ electrophstat/setup/sensor_setup.py
@@ -6,9 +6,13 @@ def init_sensors(self):
     self.pH_calibrate_dialog = CalibratepHDialog(float(self.pH_calibration_low),float(self.pH_calibration_mid),float(self.pH_calibration_high),self)
 
         
+    #slots = {
+    #        "pH":   [self.button_cont.update_gui],
+    #        "temperature": [self.button_cont.update_gui],
+    #}
+
     slots = {
             "pH":   [self.button_cont.update_gui],
-            "temperature": [self.button_cont.update_gui],
     }
 
     self.sensor_ctrl = SensorController(self, update_slots=slots, interval=1.0)
