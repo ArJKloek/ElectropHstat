11f42d354ccf641427b939a0ac4e23f07b4976f7
ArJKloek <123403478+ArJKloek@users.noreply.github.com>
2025-05-25 14:29:38 +0200

    Update adc_setup.py

--- electrophstat/setup/adc_setup.py
+++ electrophstat/setup/adc_setup.py
@@ -1,4 +1,12 @@
 from electrophstat.gui.adc_controller import ADCController
 
+
 def init_adc(self):
-    self.adc_ctrl = ADCController(self, channel=1, interval=1.0)
+    
+    if self.enable_turbidity_sensor:
+
+        self.adc_ctrl = ADCController(self, channel=1, interval=1.0)
+    else:
+        self.adc_ctr = None
+        self.TurbidityGroup.setVisible(False)
+        self.logging_ctrl.disable_logging(['turbidity'])
