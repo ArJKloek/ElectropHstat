8823a22953e873821b8af37f735ccec23c666b5f
ArJKloek <123403478+ArJKloek@users.noreply.github.com>
2025-05-25 11:14:17 +0200

    Update logging_setup.py

--- electrophstat/setup/logging_setup.py
+++ electrophstat/setup/logging_setup.py
@@ -10,8 +10,8 @@ def init_logger(win):
         
     win.logger = Logger(
         base_dir=LOG_BASE,
-        labels=["pump","pH", "temperature","voltage","current", "coulomb"],
-        column_names = ["Pumped (ml)", "pH", "Temperature (°C)", "Voltage (V)", "Current (A)", "Coulomb (C)"]
+        labels=["pump","pH", "temperature","voltage","current", "coulomb", "turbidity"],
+        column_names = ["Pumped (ml)", "pH", "Temperature (°C)", "Voltage (V)", "Current (A)", "Coulomb (C)", "Turbidity"]
 
     )
     win.logging_ctrl = LoggingController(win, interval=5.0)
