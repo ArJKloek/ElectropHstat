a1a90672cb60d1366d1170a33dfbcea6c6e06dae
ArJKloek <123403478+ArJKloek@users.noreply.github.com>
2025-05-25 13:46:19 +0200

    Update sensor_setup.py

--- electrophstat/setup/sensor_setup.py
+++ electrophstat/setup/sensor_setup.py
@@ -18,7 +18,7 @@ def init_sensors(self):
 
     self.sensor_ctrl = SensorController(self, update_slots=slots, interval=1.0)
     
-    if self.enale_ph_sensor:
+    if self.enable_ph_sensor:
         self.pH_calibrate_dialog = CalibratepHDialog(float(self.pH_calibration_low),float(self.pH_calibration_mid),float(self.pH_calibration_high),self)
         pH_worker = self.sensor_ctrl.pH_worker
         # connect your dialog
