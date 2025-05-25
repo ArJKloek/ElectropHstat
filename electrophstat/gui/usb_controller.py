bf1247cd2a079b920753f047e6273e9163ca0a8d
ArJKloek <123403478+ArJKloek@users.noreply.github.com>
2025-05-25 14:52:30 +0200

    Update usb_controller.py

--- electrophstat/gui/usb_controller.py
+++ electrophstat/gui/usb_controller.py
@@ -1,7 +1,7 @@
 # electrophstat/gui/usb_controller.py
 
 from PyQt5.QtCore import QObject, QThread, pyqtSlot
-from electrophstat.workers.usb_worker import USBWorker
+from electrophstat.io.usb_monitor import USBWorker
 
 class UsbController(QObject):
     """
