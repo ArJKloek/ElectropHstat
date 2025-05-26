f36dc3bdbb4baaa91a56e7e6d90fa496eeb8316a
ArJKloek <123403478+ArJKloek@users.noreply.github.com>
2025-05-26 23:00:23 +0200

    Update usb_worker.py

--- electrophstat/workers/usb_worker.py
+++ electrophstat/workers/usb_worker.py
@@ -18,19 +18,20 @@ class USBWorker(QObject):
     
     def __init__(self):
         super().__init__()
-       
+        self.is_running = False  # Ensure this is always defined
+
     def run(self):
-        # Background task logic
+        self.is_running = True  # Set running flag when starting
         while self.is_running:
-        
             self.monitor_usb()
-            
             time.sleep(1)  # Adjust the sleep time as needed
 
     def stop(self):
         self.is_running = False
-    
+
     def monitor_usb(self):
+        if not self.is_running:
+            return
         context = Context()
         data = []
         for device in context.list_devices(subsystem='block', DEVTYPE='partition'):
@@ -38,31 +39,27 @@ class USBWorker(QObject):
             if ('ID_BUS' in properties and properties['ID_BUS'] == 'usb'
             and 'ID_FS_TYPE' in properties
             and 'ID_USB_DRIVER' in properties
-            and properties['ID_USB_DRIVER'] == 'usb-storage'): #and self.Log_file.get():
+            and properties['ID_USB_DRIVER'] == 'usb-storage'):
                 data.append(True)
             else:
                 data.append(False)
         if True in data:
             self.path = self.mount_device()
-            self.update_usb.emit(True, self.path)  # Emit signal with a boolean and data
-
-            
+            if self.is_running:  # Double-check before emitting
+                self.update_usb.emit(True, self.path)
         else:
-            self.update_usb.emit(False,"")            
-          
+            if self.is_running:  # Double-check before emitting
+                self.update_usb.emit(False, "")
+
     def mount_device(self):
         context = Context()
-    
-        removable = [device for device in context.list_devices(subssystem='block', DEVTYPE='disk') if device.attributes.asstring('removable')=="1"]
+        removable = [device for device in context.list_devices(subsystem='block', DEVTYPE='disk') if device.attributes.asstring('removable') == "1"]
         partition = []
         for device in removable:
             partitions = [device.device_node for device in device.parent.children if 'ID_FS_TYPE' in device.properties]
-        
             if partitions:
                 partition = partitions[0]
-
                 result = subprocess.run(['findmnt', '-n', '-o', 'TARGET', partition], stdout=subprocess.PIPE, text=True)
                 mount_point = result.stdout.strip()
-            
                 if mount_point:
                     return mount_point
