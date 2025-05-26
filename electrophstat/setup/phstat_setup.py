50ad6c2c812201ad547e88e6dc68dc68b3542a20
ArJKloek <123403478+ArJKloek@users.noreply.github.com>
2025-05-26 22:23:48 +0200

    Update phstat_setup.py

--- electrophstat/setup/phstat_setup.py
+++ electrophstat/setup/phstat_setup.py
@@ -3,7 +3,7 @@ from electrophstat.connections.pHstat_connections import pHStatConnections
 from electrophstat.gui.phstat_controller import pHStatController
 from electrophstat.gui.dialogs import CalibratePumpDialog
 from electrophstat.hardware import discover_switcher
-#from electrophstat.dummy.dummy_switcher import MockLib8MosInd
+from electrophstat.dummy.dummy_switcher import MockLib8MosInd
 
 #lib8mosind = MockLib8MosInd()
 
@@ -13,8 +13,20 @@ def init_phstat(self):
     if self.enable_phstat:
         self.calibrate_pump_dialog = CalibratePumpDialog(float(self.pump_volume_per_cycle_ml), float(self.pump_cycle_duration_s), self)
         hw = discover_switcher()
-        print(f"Using hardware: {hw}")
-        
+        if isinstance(hw, MockLib8MosInd):
+            # If dummy power supply, color the groupbox orange
+            # to indicate that it is a dummy.
+            group_name = "pHstatGroup"
+            # grab the widget from your MainWindow
+            groupbox = getattr(self, group_name, None)
+            if groupbox is not None:
+                groupbox.setStyleSheet("""
+                    QGroupBox {
+                    border: 2px solid orange;
+                    background-color: #FFF8E1;
+                    }
+                """)
+               
         self.pump_ctrl = PumpController(
                 hw=hw,
                 logger=self.logger,
