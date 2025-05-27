4601669f24cba3cc476d8a28ad8cdbd2fd76e53a
ArJKloek <123403478+ArJKloek@users.noreply.github.com>
2025-05-27 15:49:58 +0200

    Update test_dummy_pps.py

--- tests/test_dummy_pps.py
+++ tests/test_dummy_pps.py
@@ -4,6 +4,6 @@ from electrophstat.dummy.dummy_pps import DummyPPS
 
 def test_dummy_pps_basic():
     ps = DummyPPS(); ps.connect()
-    ps.set_voltage(5); ps.set_current(0.3); ps.set_output(True)
+    ps.voltage(5); ps.current(0.3); ps.output(True)
     v, a, mode = ps.read_output()
     assert abs(v-5) < 0.1 and abs(a-0.3) < 0.05
