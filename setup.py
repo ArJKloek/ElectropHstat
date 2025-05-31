08497a34462059b34c41b7fe84da189644b77d31
ArJKloek <123403478+ArJKloek@users.noreply.github.com>
2025-05-31 21:20:19 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,16 @@ from setuptools import setup, find_packages
 setup(
     name="electrophstat",
     version="0.1.0",
+    author="Arjan Kloekhorst",
     packages=find_packages(exclude=["tests*"]),
     python_requires=">=3.7",
     install_requires=[
         "PyQt5",
-        "pyserial",
-        "smbus2",
+        "pyqtgraph",         # For plotting
+        "pyserial",          # For serial communication
+        "smbus2",            # For I2C communication
+        "numpy",             # Used in calibration/modeling
+        "scipy",             # Used in model fitting
         "typing_extensions; python_version < '3.8'",
     ],
     extras_require={
