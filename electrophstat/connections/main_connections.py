96ffa567bb9612b5878df83754ae0704f0221249
ArJKloek <123403478+ArJKloek@users.noreply.github.com>
2026-09-10 10:52:19 +0200

    Update main_connections.py

--- electrophstat/connections/main_connections.py
+++ electrophstat/connections/main_connections.py
@@ -65,7 +65,6 @@ def setup_mainwindow_signals(win):
 
     # Map interval to QAction name
     interval_to_action = {
-        5: "action15_sec",
         15: "action15_sec",
         30: "action30_sec",
         60: "action1_min",
